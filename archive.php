c443e61e17ad950b867a38b4c2e454b0e918e3cf
Satrya <satryabima24@gmail.com>
2014-12-31 02:22:44 +0700

    Use new archive title tag

--- archive.php
+++ archive.php
@@ -8,51 +8,16 @@
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h2 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'delivery' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'delivery' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'delivery' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'delivery' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'delivery' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'delivery' ) ) . '</span>' );
-
-						else :
-							_e( 'Archives', 'delivery' );
-
-						endif;
-					?>
-				</h2>
 				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
+					the_archive_title( '<h2 class="page-title">', '</h2>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
 
 			<?php endwhile; ?>
 
